64fc82bcb3dc2f808abc617a66da6d30d1fa7185
Shraddha16 <120588579+shraddha160902@users.noreply.github.com>
2024-03-13 21:58:30 +0530

    Initial commit

---
+++ enums/src/enums/com/DayOfTheWeek.java
@@ -0,0 +1,6 @@
+package enums.com;
+
+public enum DayOfTheWeek {
+
+	sunday,monday,tuesday,wednesday,thursday,friday,saturday;
+}
---
+++ enums/src/enums/com/enumdemo.java
@@ -0,0 +1,15 @@
+package enums.com;
+
+public class enumdemo {
+
+	public static void main(String[] args) {
+		 	
+		DayOfTheWeek day=DayOfTheWeek.thursday;
+		if(day==DayOfTheWeek.thursday)
+		{
+			System.out.println("Yay! It's almost friday!!");
+		}
+
+	}
+
+}
